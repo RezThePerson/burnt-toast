c86fb1b060f6695bb9cb34a7f56329122c7d962f
Rez <reztheperson@proton.me>
2026-03-15 09:55:46 +0530

    add generaing the local sytem's hardware configs

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ echo "you must be using nix os to use this rice"
 
 cd ~
 git clone https://github.com/RezThePerson/burnt-toast.git
+nixos-generate-config --show-hardware-config > ./burnt-toast/nixos/host/hardware-configuration.nix
 cd burnt-toast/dotfiles
 stow -t ~/ *
 sudo nixos-rebuild switch --flake ~/Burnt-Toast/nixos#nixos
